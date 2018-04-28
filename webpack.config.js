454e51c179bbd99d846a15e3d9e055d434928796
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2018-04-28 20:28:34 +0100

    Switch to Webpack and remove RequireJS
    This achieves feature parity with the existing code. From here, we can
    now move to CommonJS require statements, and then perhaps towards
    Typescript.

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+var path = require('path');
+
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var GitRevisionPlugin = require('git-revision-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin');
+
+
+const OUTPUT_DIRECTORY = 'dist';
+
+function recursivelyCopy(dir) {
+  return {from: dir, to: dir, toType: 'dir'};
+}
+
+function cleanUpLeftovers() {
+  return new CleanWebpackPlugin(OUTPUT_DIRECTORY, {});
+}
+
+function copyStaticAssets() {
+  return new CopyWebpackPlugin([
+    recursivelyCopy('css'),
+    recursivelyCopy('images'),
+    recursivelyCopy('sprites'),
+    recursivelyCopy('thirdparty'),
+    'LICENSE',
+    'COPYING',
+  ]);
+}
+
+function injectBundleIntoHTML(gitHash) {
+  return new HtmlWebpackPlugin({
+    gitHash,
+    inject: true,
+    hash: true,
+    template: './index.html',
+    filename: 'index.html'
+  });
+}
+
+function injectBuildIdIntoAbout(gitHash) {
+  return new HtmlWebpackPlugin({
+    gitHash,
+    inject: false,
+    hash: true,
+    template: './about.html',
+    filename: 'about.html'
+  });
+}
+
+function deferInjectedBundle() {
+  return new ScriptExtHtmlPlugin({
+    defaultAttribute: 'defer'
+  });
+}
+
+function addDevelopmentConfigTo(options) {
+  options.devServer = {
+    contentBase: `./${OUTPUT_DIRECTORY}`
+  };
+
+  options.devtool = 'source-maps';
+  options.mode = 'development';
+}
+
+function getBuildId() {
+  // Technically don't need to use the webpack plugin, as not passing it to Webpack...
+  const gitPlugin = new GitRevisionPlugin({
+    commitHashCommand: `log -1 --pretty=format:'%h' master`
+  });
+
+  return gitPlugin.commithash().slice(0, 12);
+}
+
+module.exports = function(env, argv) {
+  const buildId = getBuildId();
+
+  const options = {
+    entry: './js/Main.js',
+    output: {
+      path: path.resolve(__dirname, OUTPUT_DIRECTORY),
+      filename: 'main.js'
+    },
+    plugins: [
+      cleanUpLeftovers(),
+      copyStaticAssets(),
+      injectBundleIntoHTML(buildId),
+      injectBuildIdIntoAbout(buildId),
+      deferInjectedBundle()
+    ]
+  };
+
+  if (env.development) {
+    addDevelopmentConfigTo(options);
+  }
+
+  return options;
+};
