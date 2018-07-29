fa2ec9bd30731495ab41a9b31f0fdf93e81f1c9e
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2018-07-29 18:23:21 +0100

    Introduce a new position class that does not perform bounds checking

--- src/position.ts
+++ src/position.ts
@@ -1,4 +1,42 @@
+import * as Direction from "./direction";
+
+class DirectionDelta {
+
+    constructor(readonly xDelta: number, readonly yDelta: number) {}
+}
+
+type MovementDirection = Direction.Direction;
+
+function getDeltaFor(direction: MovementDirection): DirectionDelta {
+    switch (direction) {
+        case Direction.NORTH:
+            return new DirectionDelta(0, -1);
+        case Direction.NORTHEAST:
+            return new DirectionDelta(1, -1);
+        case Direction.EAST:
+            return new DirectionDelta(1, 0);
+        case Direction.SOUTHEAST:
+            return new DirectionDelta(1, 1);
+            case Direction.SOUTH:
+            return new DirectionDelta(0, 1);
+        case Direction.SOUTHWEST:
+            return new DirectionDelta(-1, 1);
+            case Direction.WEST:
+            return new DirectionDelta(-1, 0);
+        case Direction.NORTHWEST:
+            return new DirectionDelta(-1, -1);
+        default:
+            throw new Error(`Unexpected direction!`);
+    }
+}
+
 export class Position {
+
+    static move(position: Position, direction: MovementDirection): Position {
+        const {x, y} = position;
+        const {xDelta, yDelta} = getDeltaFor(direction);
+        return new Position(x + xDelta, y + yDelta);
+    }
 
     static origin(): Position {
         return new Position(0, 0);
--- test/position.ts
+++ test/position.ts
@@ -1,3 +1,4 @@
+import * as Direction from "../src/direction";
 import { Position } from "../src/position";
 
 describe("the Position class", () => {
@@ -9,6 +10,89 @@ describe("the Position class", () => {
 
             expect(originX).toBe(0);
             expect(originY).toBe(0);
+        });
+    });
+
+    describe("when moving", () => {
+
+        it("should apply the correct transformation when moving north", () => {
+            const originalPos = new Position(2, 2);
+
+            const movedPos = Position.move(originalPos, Direction.NORTH);
+
+            expect(movedPos.x).toBe(originalPos.x);
+            expect(movedPos.y).toBe(originalPos.y - 1);
+        });
+
+        it("should apply the correct transformation when moving northeast", () => {
+            const originalPos = new Position(3, 4);
+
+            const movedPos = Position.move(originalPos, Direction.NORTHEAST);
+
+            expect(movedPos.x).toBe(originalPos.x + 1);
+            expect(movedPos.y).toBe(originalPos.y - 1);
+        });
+
+        it("should apply the correct transformation when moving east", () => {
+            const originalPos = new Position(1, 2);
+
+            const movedPos = Position.move(originalPos, Direction.EAST);
+
+            expect(movedPos.x).toBe(originalPos.x + 1);
+            expect(movedPos.y).toBe(originalPos.y);
+        });
+
+        it("should apply the correct transformation when moving southeast", () => {
+            const originalPos = new Position(5, 11);
+
+            const movedPos = Position.move(originalPos, Direction.SOUTHEAST);
+
+            expect(movedPos.x).toBe(originalPos.x + 1);
+            expect(movedPos.y).toBe(originalPos.y + 1);
+        });
+
+        it("should apply the correct transformation when moving south", () => {
+            const originalPos = new Position(4, 2);
+
+            const movedPos = Position.move(originalPos, Direction.SOUTH);
+
+            expect(movedPos.x).toBe(originalPos.x);
+            expect(movedPos.y).toBe(originalPos.y + 1);
+        });
+
+        it("should apply the correct transformation when moving southwest", () => {
+            const originalPos = new Position(7, 3);
+
+            const movedPos = Position.move(originalPos, Direction.SOUTHWEST);
+
+            expect(movedPos.x).toBe(originalPos.x - 1);
+            expect(movedPos.y).toBe(originalPos.y + 1);
+        });
+
+        it("should apply the correct transformation when moving west", () => {
+            const originalPos = new Position(4, 6);
+
+            const movedPos = Position.move(originalPos, Direction.WEST);
+
+            expect(movedPos.x).toBe(originalPos.x - 1);
+            expect(movedPos.y).toBe(originalPos.y);
+        });
+
+        it("should apply the correct transformation when moving northwest", () => {
+            const originalPos = new Position(8, 4);
+
+            const movedPos = Position.move(originalPos, Direction.NORTHWEST);
+
+            expect(movedPos.x).toBe(originalPos.x - 1);
+            expect(movedPos.y).toBe(originalPos.y - 1);
+        });
+
+        it("should throw an error for an unexpected direction", () => {
+            const position = new Position(31, 11);
+
+            const apiAbuse = () => Position.move(position, undefined as any);
+
+            expect(apiAbuse).toThrow();
         });
     });
 
